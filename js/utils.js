887a474425b54ec2b3e199ba12f164e2edf862e6
mr-bansal <121489397+mr-bansal@users.noreply.github.com>
2024-06-12 00:18:43 +0530

    Updated some texts shown

--- js/utils.js
+++ js/utils.js
@@ -1,11 +1,11 @@
 function rectangularCollision({ rectangle1, rectangle2 }) {
   return (
     rectangle1.attackBox.position.x + rectangle1.attackBox.width >=
-      rectangle2.position.x &&
+    rectangle2.position.x &&
     rectangle1.attackBox.position.x <=
-      rectangle2.position.x + rectangle2.width &&
+    rectangle2.position.x + rectangle2.width &&
     rectangle1.attackBox.position.y + rectangle1.attackBox.height >=
-      rectangle2.position.y &&
+    rectangle2.position.y &&
     rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
   )
 }
@@ -14,7 +14,7 @@ function determineWinner({ player, enemy, timerId }) {
   clearTimeout(timerId)
   document.querySelector('#displayText').style.display = 'flex'
   if (player.health === enemy.health) {
-    document.querySelector('#displayText').innerHTML = 'Tie'
+    document.querySelector('#displayText').innerHTML = 'Match is Tied'
   } else if (player.health > enemy.health) {
     document.querySelector('#displayText').innerHTML = 'Player 1 Wins'
   } else if (player.health < enemy.health) {
